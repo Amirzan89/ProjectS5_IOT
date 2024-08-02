3ea474bc1142a523b10b475632ca0178aea5831d
Amirzan Fikri Prasetyo <102281081+Amirzan89@users.noreply.github.com>
2024-08-02 20:17:52 +0700

    neww

---
+++ projectS5Esp/projectS5Esp.ino
@@ -0,0 +1,11 @@
+#include "config.ino"
+#include "utils.ino"
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ projectS5Ino/server.ino
@@ -0,0 +1,8 @@
+#define <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <SPIFFS.h>
+//const char* ssid = "MyHotspot";
+//const char* password = "password";
+void initServer(){
+  //
+}
